0bc3cdd4718486e89530847d01c85d70a31f4edd
Gadaffi-24 <ST10495452@rcconnect.edu.za>
2025-04-10 07:50:05 +0000

    AndroidTest

--- MealSuggestion6/app/src/test/java/com/example/mealsuggestion/ExampleUnitTest.kt
+++ MealSuggestion6/app/src/test/java/com/example/mealsuggestion/ExampleUnitTest.kt
@@ -26,30 +26,3 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 
-@RunWith(AndroidJUnit4::class)
-class MainActivityTest {
-    private lateinit var activityScenario: ActivityScenario<MainActivity>
-    @Before
-    fun setup() {
-// Launch the activity before each test
-        activityScenario = ActivityScenario.launch(MainActivity::class.java)
-    }
-
-
-    @Test
-    fun testButtonClickUpdatesTextView() {
-        activityScenario.onActivity { activity ->
-// Set up references to UI elements
-            val button: Button = activity.findViewById(R.id.suggestButton)
-            val textView: TextView = activity.findViewById(R.id.mealSuggestion)
-            val editText: EditText = activity.findViewById(R.id.timeInput)
-// Simulate user input
-            editText.setText("John")
-// Perform button click
-            button.performClick()
-// Check if the TextView's text was updated
-            assertEquals("Hello, John! Button was clicked!", textView.text)
-            assertEquals(Color.RED, textView.currentTextColor)
-        }
-    }
-}
